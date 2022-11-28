2c55cdae3c1a6beb58443d04d248713c1d5f2b95
kuldeep <kuldipaavashist@gmail.com>
2022-11-28 19:42:03 +0530

    Initial Commit

---
+++ OneScreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OneScreen
+//
+//  Created by Kuldeep Vashisht on 28/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OneScreen/OneScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneScreenApp.swift
+//  OneScreen
+//
+//  Created by Kuldeep Vashisht on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct OneScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
